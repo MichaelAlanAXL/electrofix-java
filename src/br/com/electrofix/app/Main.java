fccdf633d3654a3502a405d793c277bb3ee2cc3f
MichaelAlanAXL <michael.dtna@gmail.com>
2025-08-05 19:21:08 -0300

    add Main.java with initial structure

---
+++ src/br/com/electrofix/app/Main.java
@@ -0,0 +1,8 @@
+package br.com.electrofix.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== ElectroFix Assistência Técnica ===");
+
+    }
+}
